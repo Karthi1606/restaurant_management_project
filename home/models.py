1a8348e25ed92cbe43ef623b4b7c7c39a8be6cf1
Karthi1606 <146052515+Karthi1606@users.noreply.github.com>
2025-08-04 19:13:23 +0530

    17569

--- home/models.py
+++ home/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class UserProfile(model.Model):
+    user = models.oneToOneField(User, on_delete=models.CASECADE, related_name='profile')
+    phone_number = models.CharFeild(max_length=15, blank=True)
+
+    def __str__(self):
+        return self.user.get_full_name() or self.user.username
