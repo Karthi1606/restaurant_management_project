f733efc33847a8a71a5e3d699b5b7ff1e178c293
Karthi1606 <146052515+Karthi1606@users.noreply.github.com>
2025-08-06 17:30:17 +0530

    Auto-save orders/models.py

--- orders/models.py
+++ orders/models.py
@@ -1,4 +1,19 @@
 from django.db import models
 
 # Create your models here.
-class 
+class Order(models.Model):
+    STATUS_CHOICES = [
+        ('PENDING','pending')
+        ('PROCESSING','processing')
+        ('COMPLETED','comledeted')
+        ('CANCELLED','cancelled')
+    ]
+
+    customer = models.ForeingKey(User, On_delete=models.CASECADE, related_name='orders')
+    order_item = models.ManyToManyFiled(Menu, related_name='orders')
+    total_amount = models.Decimal.Field(max_length=10, decimal_places=2)
+    oreder_status = model.CharField(mac_length=20, choices=STATUS_CHOICES, default='PENDING')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Order #{self.id} by {self.customer.username}
