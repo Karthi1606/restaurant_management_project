bbb399619eed9a4e82eeb843d90ea4cfe0596222
Karthi1606 <146052515+Karthi1606@users.noreply.github.com>
2025-08-06 17:03:32 +0530

    Auto-save account/models.py

--- account/models.py
+++ account/models.py
@@ -1,3 +1,10 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+class Menu(models.Model):
+    name = models.Charfield(maxlenth=100)
+    description = model.TextField(blank=True)
+    price = models.DecimalField(max_digit=8, decimal_places=2) 
+
+    def __str__(self):
+        return self.name
